3c5d708617f20612fc5f02496800046557b502a4
auditorydamage <auditorydamage@gmail.com>
2026-07-27 10:40:54 -0230

    Updated Account model
    now includes subschemas for User and Provider account types.

--- WellnessHub/models/account.model.js
+++ WellnessHub/models/account.model.js
@@ -1,6 +1,64 @@
 const mongoose = require('mongoose');
 
-const accountSchema = mongoose.Schema({
+const UserDataSchema = mongoose.Schema({
+    userPic: {
+        type: String,
+        required: false
+    },
+    height: {
+        type: Number,
+        unit: String,
+        required: true
+    },
+    activityLevel: {
+        type: String,
+        enum: ["sedentary", "lightly active", "moderately active", "very active", "extra active"],
+        required: true
+    },
+    weightLog: {
+        type: [WeightLogSchema],
+        required: false
+    },
+    moodLog: {
+        type: [MoodLogSchema],
+        required: false
+    },
+    waterLog: {
+        type: [WaterLogSchema],
+        required: false
+    },
+    heartRateLog: {
+        type: [HeartRateLogSchema],
+        required: false
+    },
+    activityLog: {
+        type: [ActivityLogSchema],
+        required: false
+    },
+    mealPlans: {
+        type: [MealPlanSchema],
+        required: false
+    }
+});
+
+const ProviderDataSchema = mongoose.Schema({
+    userPic: {
+        type: String,
+        required: false
+    },
+    bio: {
+        type: String,
+        required: false
+    },
+    speciality: {
+        type: String,
+        required: false
+    }
+});
+
+const AdminDataSchema = mongoose.Schema({});
+
+const AccountSchema = mongoose.Schema({
     username: {
         type: String,
         required: true,
@@ -14,14 +72,14 @@ const accountSchema = mongoose.Schema({
         enum: ["user", "provider", "admin"],
         required: true
     },
-    creationDate: {
-        type: Number,
-        required: true
-    },
-    $cond: {
-        if: { $eq: []}
+    $switch: {
+        branches: [
+            { case: {$eq: [ "accountType", "admin" ]}, then: {adminData: AdminDataSchema}},
+            { case: {$eq: [ "accountType", "provider" ]}, then: {providerData: ProviderDataSchema}}
+        ],
+        default: { userData: UserDataSchema }
     }
-});
+}, { timestamps: true });
 
 const Account = mongoose.model("wh_account", AccountSchema);
 module.exports = Account;
