18d5a138f880864cf4fdff9c9156ceef485c35e4
anuthomson <anumol.thomas94@ed.cna.nl.ca>
2026-08-10 14:11:14 -0230

    Add sleep tracking CRUD endpoints

--- WellnessHub/app.js
+++ WellnessHub/app.js
@@ -17,13 +17,17 @@ const waterRoutes = require("./routes/water.routes");
 const moodRoutes = require("./routes/mood.routes");
 const walkingRoutes = require("./routes/walking.routes");
 const heartrateRoutes = require("./routes/heartrate.routes");
+const sleepRoutes = require("./routes/sleep.routes");
 
 app.use("/api/mood", moodRoutes);
 app.use("/api/walking", walkingRoutes);
 app.use("/api/heartrate", heartrateRoutes);
+app.use("/api/sleep", sleepRoutes);
 app.use("/api/articles", articleRoutes);
 app.use("/api/accounts", accountRoutes);
 app.use("/api/water", waterRoutes);
+
+
 // test GET
 app.get('/', (req, res) => {
   res.send('GET works!');
