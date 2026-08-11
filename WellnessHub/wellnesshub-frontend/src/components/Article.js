3578e29aa64e41d22188abb8d41fc2774e6ab439
auditorydamage <auditorydamage@gmail.com>
2026-08-11 14:40:21 -0230

    Updates to article model and Article components
    The entry stuff is in place; just need to test with working authorization now

--- WellnessHub/wellnesshub-frontend/src/components/Article.js
+++ WellnessHub/wellnesshub-frontend/src/components/Article.js
@@ -3,7 +3,7 @@ import { useState, useEffect } from 'react';
 
 function ArticleDisplay ({articleId}) {
 
-    const [ content, setContent ] = useState({articleId: "", author: "", title: "", category: "", activity: {activityType: "", body: ""}, meal: {name: "", cuisine: "", mealType: "", body: "", ingredients: []}});
+    const [ content, setContent ] = useState({articleId: "", author: "", title: "", category: "activity"});
     
     useEffect(() => {
         async function fetchContent() {
@@ -13,6 +13,8 @@ function ArticleDisplay ({articleId}) {
                 setContent(fetchedArticle.article);
             } catch (error) {
                 console.error("Error fetching article content:", error);
+                alert(`Error fetching article content. Please try again.
+                       Error: ${error.message}`);
             }
         }
         fetchContent();
@@ -41,6 +43,11 @@ function ArticleEditor () {
         setContent({...content, title: e.target.value});
     }
 
+    function handleSubheadChange(e) {
+        e.preventDefault();
+        setContent({...content, subhead: e.target.value});
+    }
+
     function handleCategoryChange(e) {
         e.preventDefault();
         setContent({...content, category: e.target.value});
@@ -48,13 +55,45 @@ function ArticleEditor () {
 
     function handleBodyChange(e) {
         e.preventDefault();
-        setContent({...content, activity: {...content.activity, body: e.target.value}});
+        if (content.category === "activity") {
+            setContent({...content, activity: {...content.activity, body: e.target.value}});
+        } else if (content.category === "meal") {
+            setContent({...content, meal: {...content.meal, body: e.target.value}});
+        }
     }
 
-    function handleSubmit(e) {
+    function handleActivityTypeChange(e) {
         e.preventDefault();
-        // Insert correct submission logic once endpoints are in place.
-        console.log("Submitted content: ", {content});
+        setContent({...content, activity: {...content.activity, activityType: e.target.value}});
+    }
+
+    function handleMealTypeChange(e) {
+        e.preventDefault();
+        setContent({...content, meal: {...content.meal, mealType: e.target.value}});
+    }
+
+    function handleCuisineChange(e) {
+        e.preventDefault();
+        setContent({...content, meal: {...content.meal, cuisine: e.target.value}});
+    }
+
+    async function handleSubmit(e) {
+        e.preventDefault();
+            const result = await fetch("http://localhost:3000/api/articles", {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json"
+                },
+                body: JSON.stringify(content)
+            });
+            if (!result.ok) {
+                console.error("Error submitting article: ", result.statusText);
+                alert(`Error submitting article. Please try again.
+                       Error: ${result.statusText}`);
+            } else {
+                alert("Article submitted successfully!");
+                console.log("Submitted content: ", {content});
+        }
     }
     
     return (
@@ -65,21 +104,43 @@ function ArticleEditor () {
                         <tbody>
                         <tr>
                             <td><label>Category:</label></td>
-                            <td><select value={content.category} onChange={handleCategoryChange}>
-                                <option value="activity">Activity</option>
+                            <td><select value={content.category} defaultValue="activity" onChange={handleCategoryChange}>
                                 <option value="meal">Meal</option>
+                                <option value="activity">Activity</option>
                             </select></td>
                         </tr>
+                        { content.category === "activity" ?
+                            <>
+                            <tr>
+                                <td><label>Activity type:</label></td>
+                                <td><input type="text" name="activity.activityType" value={content.activity.activityType} onChange={handleActivityTypeChange} /></td>
+                            </tr>
+                            </>
+                         : 
+                         <>
+                            <tr>
+                                <td><label>Meal type:</label></td>
+                                <td><input type="text" name="meal.mealType" value={content.meal.mealType} onChange={handleMealTypeChange} /></td>
+                            </tr>
+                            <tr>
+                                <td><label>Cuisine:</label></td>
+                                <td><input type="text" name="meal.cuisine" value={content.meal.cuisine} onChange={handleCuisineChange} /></td>
+                            </tr>
+                            </> }
                         <tr>
                             <td><label>Title:</label></td>
                             <td><input type="text" name="title" value={content.title} onChange={handleTitleChange} /></td>
+                        </tr>
+                        <tr>
+                            <td><label>Subhead:</label></td>
+                            <td><input type="text" name="subhead" value={content.subhead} onChange={handleSubheadChange} /></td>
                         </tr>
                         </tbody>
                     </table>
                     <label>Enter content here in Markdown format:</label>
                     <textarea 
-                        name="activity.body"
-                        value={content.activity.body}
+                        name="body"
+                        value={content.category === "activity" ? content.activity.body : content.meal.body}
                         onChange={handleBodyChange}
                         rows="30"
                         cols="100"
@@ -89,7 +150,7 @@ function ArticleEditor () {
             </div>
             <div className="article-preview">
                 <p>Preview:</p>
-                <Markdown>{content.activity.body}</Markdown>
+                <Markdown>{content.category === "activity" ? content.activity.body : content.meal.body}</Markdown>
             </div>
         </>
     );
